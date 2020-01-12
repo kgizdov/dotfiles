1c0c7902789450b14c37f4c47075813adefcac69
Konstantin Gizdov <kgizdov@gmail.com>
2020-01-12 15:37:29 +0200

    waybar: keyboard indicators now show country flags

--- .config/waybar/scripts/code_keyboard_layout.sh
+++ .config/waybar/scripts/code_keyboard_layout.sh
@@ -3,10 +3,13 @@
 # for WASD Code Keyboar over USB
 layout_name=$(swaymsg -t get_inputs | jq '.[] | select(.identifier=="1241:361:USB_Keyboard") | .xkb_active_layout_name')
 # echo "${layout_name}"
+# 3 return options encode 3 intervals from 0% to 100%
+# namely 0-32, 33-66, 67-100
+# this value is used for selecting an icon to display
 if [ "${layout_name}" = '"Bulgarian (new phonetic)"' ]; then
   echo '{"text": "bg", "tooltip": "", "class": "", "percentage": 0}' | jq --unbuffered --compact-output
 elif [ "${layout_name}" = '"English (US)"' ]; then
-  echo '{"text": "us", "tooltip": "", "class": "", "percentage": 0}' | jq --unbuffered --compact-output
+  echo '{"text": "us", "tooltip": "", "class": "", "percentage": 50}' | jq --unbuffered --compact-output
 else
-  echo '{"text": "--", "tooltip": "", "class": "", "percentage": 0}' | jq --unbuffered --compact-output
+  echo '{"text": "--", "tooltip": "", "class": "", "percentage": 100}' | jq --unbuffered --compact-output
 fi
--- .config/waybar/scripts/dell_keyboard_layout.sh
+++ .config/waybar/scripts/dell_keyboard_layout.sh
@@ -3,10 +3,13 @@
 # for built-in Dell XPS keyboard
 layout_name=$(swaymsg -t get_inputs | jq '.[] | select(.identifier=="1:1:AT_Translated_Set_2_keyboard") | .xkb_active_layout_name')
 # echo "${layout_name}"
+# 3 return options encode 3 intervals from 0% to 100%
+# namely 0-32, 33-66, 67-100
+# this value is used for selecting an icon to display
 if [ "${layout_name}" = '"Bulgarian (new phonetic)"' ]; then
   echo '{"text":"bg", "tooltip": "", "class": "", "percentage": 0}' | jq --unbuffered --compact-output
 elif [ "${layout_name}" = '"English (UK)"' ]; then
-  echo '{"text":"uk", "tooltip": "", "class": "", "percentage": 0}' | jq --unbuffered --compact-output
+  echo '{"text":"uk", "tooltip": "", "class": "", "percentage": 50}' | jq --unbuffered --compact-output
 else
-  echo '{"text":"--", "tooltip": "", "class": "", "percentage": 0}' | jq --unbuffered --compact-output
+  echo '{"text":"--", "tooltip": "", "class": "", "percentage": 100}' | jq --unbuffered --compact-output
 fi
